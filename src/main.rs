4a7c47f868e774bfd9df2876eda0564fe573a2dd
Matthias Kluth <kluthmatthias@gmail.com>
2026-05-14 17:48:05 +0200

    feat(memory): implement bitmap frame allocator for O(1) deallocation

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset.into_option().unwrap());
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe {
-        memory::BootInfoFrameAllocator::init(&boot_info.memory_regions)
+        memory::BitmapFrameAllocator::init(&boot_info.memory_regions, phys_mem_offset)
     };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator)
--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,8 @@
 use x86_64::{
-    structures::paging::{PageTable, OffsetPageTable},
+    structures::paging::{PageTable, OffsetPageTable, FrameAllocator, PhysFrame, Size4KiB, FrameDeallocator},
     VirtAddr, PhysAddr,
 };
+use bootloader_api::info::{MemoryRegions, MemoryRegionKind};
 
 /// Initializes a new OffsetPageTable.
 ///
@@ -34,51 +35,116 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     &mut *page_table_ptr
 }
 
-use x86_64::structures::paging::{FrameAllocator, PhysFrame, Size4KiB};
-use bootloader_api::info::{MemoryRegions, MemoryRegionKind};
-
-/// A FrameAllocator that returns usable frames from the bootloader's memory map.
-/// It implements a simple bump allocator that allocates frames sequentially from
-/// the available memory regions.
-pub struct BootInfoFrameAllocator {
-    memory_map: &'static MemoryRegions,
-    next: usize,
+pub struct BitmapFrameAllocator {
+    bitmap: &'static mut [u8],
+    max_frame_index: usize,
+    next_search_index: usize,
 }
 
-impl BootInfoFrameAllocator {
-    /// Create a FrameAllocator from the passed memory map.
+impl BitmapFrameAllocator {
+    /// Create a BitmapFrameAllocator from the passed memory map.
     ///
     /// # Safety
     /// This function is unsafe because the caller must guarantee that the passed
-    /// memory map is valid. The main constraint is that all frames that are marked
-    /// as `Usable` must be actually unused.
-    pub unsafe fn init(memory_map: &'static MemoryRegions) -> Self {
-        BootInfoFrameAllocator {
-            memory_map,
-            next: 0,
+    /// memory map is valid and that the physical memory offset is correct.
+    pub unsafe fn init(memory_map: &'static MemoryRegions, physical_memory_offset: VirtAddr) -> Self {
+        let mut max_addr = 0;
+        for region in memory_map.iter() {
+            if region.end > max_addr {
+                max_addr = region.end;
+            }
+        }
+
+        let total_frames = (max_addr / 4096) as usize;
+        let bitmap_size = (total_frames + 7) / 8;
+
+        // Find a usable region large enough for the bitmap
+        let mut bitmap_addr = 0;
+        for region in memory_map.iter() {
+            if region.kind == MemoryRegionKind::Usable && (region.end - region.start) as usize >= bitmap_size {
+                bitmap_addr = region.start;
+                break;
+            }
+        }
+
+        if bitmap_addr == 0 {
+            panic!("Could not find a usable memory region for the frame allocator bitmap");
+        }
+
+        let bitmap_ptr = (physical_memory_offset + bitmap_addr).as_mut_ptr() as *mut u8;
+        let bitmap = core::slice::from_raw_parts_mut(bitmap_ptr, bitmap_size);
+
+        // Initialize bitmap: all frames used
+        bitmap.fill(0xFF);
+
+        let mut allocator = BitmapFrameAllocator {
+            bitmap,
+            max_frame_index: total_frames,
+            next_search_index: 0,
+        };
+
+        // Mark usable regions as free in the bitmap
+        for region in memory_map.iter() {
+            if region.kind == MemoryRegionKind::Usable {
+                let start_frame = (region.start / 4096) as usize;
+                let end_frame = (region.end / 4096) as usize;
+                for i in start_frame..end_frame {
+                    allocator.set_free(i);
+                }
+            }
+        }
+
+        // Mark the bitmap itself as used
+        let bitmap_start_frame = (bitmap_addr / 4096) as usize;
+        let bitmap_end_frame = ((bitmap_addr + bitmap_size as u64 + 4095) / 4096) as usize;
+        for i in bitmap_start_frame..bitmap_end_frame {
+            allocator.set_used(i);
+        }
+
+        // Frame 0 is usually special/reserved, mark it used just in case
+        allocator.set_used(0);
+
+        allocator
+    }
+
+    fn set_free(&mut self, frame_index: usize) {
+        if frame_index < self.max_frame_index {
+            self.bitmap[frame_index / 8] &= !(1 << (frame_index % 8));
         }
     }
 
-    /// Returns an iterator over the usable frames specified in the memory map.
-    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
-        // Get usable regions from memory map
-        let regions = self.memory_map.iter();
-        let usable_regions = regions
-            .filter(|r| r.kind == MemoryRegionKind::Usable);
-        // Map each region to its address range
-        let addr_ranges = usable_regions
-            .map(|r| r.start..r.end);
-        // Transform to an iterator of frame start addresses
-        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
-        // Create `PhysFrame` types from the start addresses
-        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    fn set_used(&mut self, frame_index: usize) {
+        if frame_index < self.max_frame_index {
+            self.bitmap[frame_index / 8] |= 1 << (frame_index % 8);
+        }
+    }
+
+    fn is_used(&self, frame_index: usize) -> bool {
+        if frame_index >= self.max_frame_index {
+            return true;
+        }
+        (self.bitmap[frame_index / 8] & (1 << (frame_index % 8))) != 0
     }
 }
 
-unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+unsafe impl FrameAllocator<Size4KiB> for BitmapFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
-        let frame = self.usable_frames().nth(self.next);
-        self.next += 1;
-        frame
+        // Linear scan starting from next_search_index
+        for i in 0..self.max_frame_index {
+            let index = (self.next_search_index + i) % self.max_frame_index;
+            if !self.is_used(index) {
+                self.set_used(index);
+                self.next_search_index = (index + 1) % self.max_frame_index;
+                return Some(PhysFrame::containing_address(PhysAddr::new((index as u64) * 4096)));
+            }
+        }
+        None
+    }
+}
+
+impl FrameDeallocator<Size4KiB> for BitmapFrameAllocator {
+    unsafe fn deallocate_frame(&mut self, frame: PhysFrame<Size4KiB>) {
+        let index = (frame.start_address().as_u64() / 4096) as usize;
+        self.set_free(index);
     }
 }
