6ad74ee3ef6d3f5d14d1725f997d960b3263e9b3
Matthias Kluth <kluthmatthias@gmail.com>
2026-05-15 13:23:22 +0200

    fix(kernel): increase heap size to 512KiB to prevent OOM during discovery

--- src/allocator.rs
+++ src/allocator.rs
@@ -27,7 +27,7 @@ impl<A> Locked<A> {
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+pub const HEAP_SIZE: usize = 512 * 1024; // 512 KiB (Increased from 100 KiB)
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
