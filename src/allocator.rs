7b02b9d583102039ae9171dab14e8a7a763b9eb1
Matthias Kluth <kluthmatthias@gmail.com>
2026-05-15 14:00:03 +0200

    fix(kernel): increase heap size to 32MiB to support large-scale hardware discovery

--- src/allocator.rs
+++ src/allocator.rs
@@ -27,7 +27,7 @@ impl<A> Locked<A> {
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 8 * 1024 * 1024; // 8 MiB (Increased from 2 MiB)
+pub const HEAP_SIZE: usize = 32 * 1024 * 1024; // 32 MiB (Increased from 8 MiB)
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
