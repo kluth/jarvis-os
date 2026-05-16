7e2cb39422165f065762475b0984e3f1f87c5b72
Matthias Kluth <kluthmatthias@gmail.com>
2026-05-16 12:59:32 +0200

    fix(kernel): overhaul stability with larger stack and detailed exception diagnostics (#61) (#62)

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ pub static BOOTLOADER_CONFIG: BootloaderConfig = {
     let mut config = BootloaderConfig::new_default();
     config.mappings.physical_memory =
         core::option::Option::Some(bootloader_api::config::Mapping::Dynamic);
-    config.kernel_stack_size = 512 * 1024; // 512 KiB
+    config.kernel_stack_size = 1024 * 1024; // 1 MiB
     config
 };
 
